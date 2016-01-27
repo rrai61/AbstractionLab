2aa75d80b5dbc032c0360388952dae1341b3fc3e
ritu <ritu@Ritu>
2016-01-27 11:46:10 -0600

    created new abstract class Course and added common properties

---
+++ src/Course.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ritu
+ */
+public abstract class Course {
+    private String courseName;
+    private String courseNumber;
+    private double credits;
+}
