a9e4fdd271e9b8401d1aacc19cfbbb1fd1055e2e
ritu <ritu@Ritu>
2016-01-27 12:57:32 -0600

    moved course class to lab1 package

--- src/lab1/AdvancedJavaCourse.java
+++ src/lab1/AdvancedJavaCourse.java
@@ -16,7 +16,6 @@ public class AdvancedJavaCourse extends Course{
         return this.getCourseName().toUpperCase();
     }
 
-
     public String getPrerequisites() {
         return prerequisites;
     }
@@ -28,15 +27,6 @@ public class AdvancedJavaCourse extends Course{
             System.exit(0);
         }
         this.prerequisites = prerequisites;
-    }
-
-    public void setCredits(double credits) {
-        if(credits < 0.5 || credits > 4.0) {
-            JOptionPane.showMessageDialog(null,
-                    "Error: credits must be in the range 0.5 to 4.0");
-            System.exit(0);
-        }
-        this.setCredits(credits);
     }
     
 }
